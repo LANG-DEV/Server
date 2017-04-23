07a540b2c800772ba4a254ec62ca773ad7329075
rettymoo <rettymoo@hotmail.com>
2017-04-22 21:51:26 -0700

    identity table schema

--- server/schema/identity.py
+++ server/schema/identity.py
@@ -1,19 +1,18 @@
-class Identity:
-    attributes = {
-        'userid': 'varchar(36)',
-        'username': 'varchar(20)',
-        'email': 'varchar(50)',
-        'password': 'varchar(20)',
-        'firstname': 'varchar(35)',
-        'lastname': 'varchar(35)',
-        'birthday': 'date',
+attributes = {
+    'userid': 'varchar(36)',
+    'username': 'varchar(20)',
+    'email': 'varchar(50)',
+    'password': 'varchar(20)',
+    'firstname': 'varchar(35)',
+    'lastname': 'varchar(35)',
+    'birthday': 'date',
 
-    }
+}
 
-    primary_keys = [
-        'userid',
-    ]
+primary_keys = [
+    'userid',
+]
 
-    foreign_keys = {
+foreign_keys = {
 
-    }
+}
